b25722090095efca1f8d2437d35f6194eb778123
Alexis <alexis.ortega@anconaautopartes.com>
2023-12-11 12:36:41 -0500

    Se mejora swagger

--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using System.Reflection;
 using System.Text;
 using System.Text.Json;
 
@@ -91,6 +92,8 @@ namespace API
                 });
 
                 x.AddSecurityRequirement(CreateApiSecurityRequirement());
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                x.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
             });
 
             return services;
