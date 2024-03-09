27994872ed432a3866a27be3116ed7ac8c286ff7
Alexis6520 <alexis_nova@outlook.com>
2024-03-09 13:58:45 -0500

    Pruebas unitarias

--- Infrastructure.UnitTests/DemoItemCreateTest.cs
+++ Infrastructure.UnitTests/DemoItemCreateTest.cs
@@ -1,11 +1,19 @@
 using Logic.Commands;
+using Logic.Handlers;
 using Logic.Validators;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Services;
+using Services.Repositories;
 
 namespace Infrastructure.UnitTests
 {
     [TestClass]
     public class DemoItemCreateTest
     {
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock = new();
+        private readonly Mock<ILogger<DemoItemCreateHandler>> _loggerMock = new();
+
         public static IEnumerable<object[]> ValidationData
         {
             get
@@ -22,12 +30,25 @@ namespace Infrastructure.UnitTests
         }
 
         [TestMethod]
-        [DynamicData(nameof(ValidationData))]
+        [DynamicData(nameof(ValidationData))] 
         public void ValidateCommand(DemoItemCreateCommand command)
         {
             var validator = new DemoItemCreateValidator();
             var result = validator.Validate(command);
             Assert.IsFalse(result.IsValid);
+        }
+
+        /// <summary>
+        /// Operación simple de creación de artículo
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task CreateDemoItemAsync()
+        {
+            _unitOfWorkMock.Setup(x => x.DemoItems).Returns(new Mock<IDemoItemRepository>().Object);
+            var handler = new DemoItemCreateHandler(_unitOfWorkMock.Object, _loggerMock.Object);
+            var command = new DemoItemCreateCommand("Doritos", 50);
+            await handler.Handle(command, CancellationToken.None);
         }
     }
 }
